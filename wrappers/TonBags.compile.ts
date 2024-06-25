e27a230531fbd2afb97a9ecc6016d85984ee3f62
gavfu <gavfu@outlook.com>
2024-06-25 17:14:04 +0800

    Add bag storage and storage provider contracts

--- wrappers/TonBags.compile.ts
+++ wrappers/TonBags.compile.ts
@@ -2,5 +2,5 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'func',
-    targets: ['contracts/ton_bags.fc'],
+    targets: ['contracts/tonbags.fc'],
 };

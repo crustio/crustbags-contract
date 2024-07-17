3764cf59ddcb177b1f47235cd7b48fcc3f13d601
KayleCoder <kalecoder@outlook.com>
2024-07-17 10:46:49 +0800

    Add place order scripts
    Signed-off-by: KayleCoder <kalecoder@outlook.com>

---
+++ scripts/placeOrderToTonBags.ts
@@ -0,0 +1,26 @@
+import { NetworkProvider } from "@ton/blueprint";
+import { Address, toNano } from "@ton/core";
+import { TonBags } from "../wrappers/TonBags";
+import { getMerkleRoot } from "../tests/merkleProofUtils";
+import { default_storage_period } from "../tests/utils";
+
+export async function run(provider: NetworkProvider) {
+    const tonBags = provider.api().open(TonBags.createFromAddress(Address.parse("kQD8ntXDohGn8GPVfxX5BF6zfgl7A_fks-8QoVcmKx5TE5M-")));
+    // TODO: send order to TonBags
+    const sender = provider.sender();
+    const dataArray = [
+        0x0BAD0010n,
+        0x60A70020n,
+        0xBEEF0030n,
+        0xDEAD0040n,
+        0xCA110050n,
+        0x0E660060n,
+        0xFACE0070n,
+        0xBAD00080n,
+        0x060D0091n
+    ];
+    const merkleRoot = getMerkleRoot(dataArray);
+    const torrentHash = BigInt('0x476848C3350EA64ACCC09218917132998267F2ABC283097082FD41D511CAF11B');
+    const fileSize = 1024n * 1024n * 10n;  // 10MB
+    await tonBags.sendPlaceStorageOrder(sender, torrentHash, fileSize, merkleRoot, toNano('0.1'), default_storage_period);
+}
